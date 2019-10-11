a95c240f0385007520a1fcf8f3e94da6eab626ff
Neophear <stiiggade@gmail.com>
2019-10-11 10:40:14 +0200

    Initial commit

---
+++ ResizePictures/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace ResizePictures
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fbdTargetFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.btnChangeFolder = new System.Windows.Forms.Button();
+            this.txtTargetFolder = new System.Windows.Forms.TextBox();
+            this.btnRun = new System.Windows.Forms.Button();
+            this.nudHeight = new System.Windows.Forms.NumericUpDown();
+            this.nudWidth = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.nudHeight)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnChangeFolder
+            // 
+            this.btnChangeFolder.Location = new System.Drawing.Point(256, 12);
+            this.btnChangeFolder.Name = "btnChangeFolder";
+            this.btnChangeFolder.Size = new System.Drawing.Size(75, 23);
+            this.btnChangeFolder.TabIndex = 0;
+            this.btnChangeFolder.Text = "Folder";
+            this.btnChangeFolder.UseVisualStyleBackColor = true;
+            this.btnChangeFolder.Click += new System.EventHandler(this.btnChangeFolder_Click);
+            // 
+            // txtTargetFolder
+            // 
+            this.txtTargetFolder.Location = new System.Drawing.Point(13, 12);
+            this.txtTargetFolder.Name = "txtTargetFolder";
+            this.txtTargetFolder.Size = new System.Drawing.Size(237, 22);
+            this.txtTargetFolder.TabIndex = 1;
+            // 
+            // btnRun
+            // 
+            this.btnRun.Location = new System.Drawing.Point(256, 42);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(75, 23);
+            this.btnRun.TabIndex = 2;
+            this.btnRun.Text = "Go";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // nudHeight
+            // 
+            this.nudHeight.Location = new System.Drawing.Point(13, 42);
+            this.nudHeight.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.nudHeight.Name = "nudHeight";
+            this.nudHeight.Size = new System.Drawing.Size(120, 22);
+            this.nudHeight.TabIndex = 3;
+            this.nudHeight.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // nudWidth
+            // 
+            this.nudWidth.Location = new System.Drawing.Point(139, 43);
+            this.nudWidth.Maximum = new decimal(new int[] {
+            999999999,
+            0,
+            0,
+            0});
+            this.nudWidth.Name = "nudWidth";
+            this.nudWidth.Size = new System.Drawing.Size(111, 22);
+            this.nudWidth.TabIndex = 4;
+            this.nudWidth.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(343, 88);
+            this.Controls.Add(this.nudWidth);
+            this.Controls.Add(this.nudHeight);
+            this.Controls.Add(this.btnRun);
+            this.Controls.Add(this.txtTargetFolder);
+            this.Controls.Add(this.btnChangeFolder);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.nudHeight)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudWidth)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FolderBrowserDialog fbdTargetFolder;
+        private System.Windows.Forms.Button btnChangeFolder;
+        private System.Windows.Forms.TextBox txtTargetFolder;
+        private System.Windows.Forms.Button btnRun;
+        private System.Windows.Forms.NumericUpDown nudHeight;
+        private System.Windows.Forms.NumericUpDown nudWidth;
+    }
+}
+
---
+++ ResizePictures/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ResizePictures
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnChangeFolder_Click(object sender, EventArgs e)
+        {
+            if(fbdTargetFolder.ShowDialog() == DialogResult.OK)
+                txtTargetFolder.Text = fbdTargetFolder.SelectedPath;
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            string resizePath = txtTargetFolder.Text + "\\Resize\\";
+            Directory.CreateDirectory(resizePath);
+            List<string> filenames = new List<string>(Directory.GetFiles(txtTargetFolder.Text));
+            List<string> newFilenames = new List<string>();
+
+            for (int i = 1; i <= filenames.Count; i++)
+                newFilenames.Add(String.Format("image{0:000}.jpg", i));
+
+            Random rnd = new Random();
+
+            foreach (string filename in Directory.GetFiles(txtTargetFolder.Text))
+            {
+                Bitmap original = new Bitmap(filename);
+                int filenameIndex = rnd.Next(0, newFilenames.Count);
+
+                if (original.Height > nudHeight.Value || original.Width > nudWidth.Value)
+                {
+                    Size oldSize = original.Size;
+                    Size newSize = original.Size;
+
+                    if (oldSize.Width > nudWidth.Value)
+                    {
+                        newSize.Width = Convert.ToInt32(nudWidth.Value);
+                        double scaling = (double)nudWidth.Value / oldSize.Width;
+                        newSize.Height = Convert.ToInt32(oldSize.Height * scaling);
+                    }
+
+                    if (oldSize.Height > nudHeight.Value)
+                    {
+                        newSize.Height = Convert.ToInt32(nudHeight.Value);
+                        double scaling = (double)nudHeight.Value / oldSize.Height;
+                        newSize.Width = Convert.ToInt32(oldSize.Width * scaling);
+                    }
+
+                    Bitmap newImage = new Bitmap(newSize.Width, newSize.Height);
+                    using (Graphics gr = Graphics.FromImage(newImage))
+                    {
+                        gr.SmoothingMode = SmoothingMode.HighQuality;
+                        gr.InterpolationMode = InterpolationMode.HighQualityBicubic;
+                        gr.PixelOffsetMode = PixelOffsetMode.HighQuality;
+                        gr.DrawImage(original, new Rectangle(0, 0, newSize.Width, newSize.Height));
+                    }
+
+                    
+
+                    newImage.Save(resizePath + newFilenames[filenameIndex], System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+                else
+                    File.Copy(filename, resizePath + newFilenames[filenameIndex]);
+
+                newFilenames.RemoveAt(filenameIndex);
+            }
+        }
+    }
+}
